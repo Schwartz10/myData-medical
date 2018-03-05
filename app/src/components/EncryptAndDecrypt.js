e9e8e392726f5ac18c4fb57f772c91eb3c033289
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-03-05 10:47:28 -0500

    working decryption

--- app/src/components/EncryptAndDecrypt.js
+++ app/src/components/EncryptAndDecrypt.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import EthCrypto from 'eth-crypto';
+import { encryptWithPublicKey, decryptWithPrivateKey } from 'eth-crypto';
 import Wallet from 'ethereumjs-wallet'
 
 class EncryptAndDecrypt extends Component {
   constructor() {
     super()
-    this.state = { files: [], publicKey: "" }
+    this.state = { files: [], publicKey: "", encrypted: '', decrypted: '' }
   }
 
   componentDidMount(){
 
   }
 
-  decrypt(e) {
+  async decrypt(e) {
     e.preventDefault()
+    //make sure to prepend the private key with 0x
     const privateKey = ''
+    const decryption = JSON.parse(this.state.encrypted)
+    const decrypted = await decryptWithPrivateKey(privateKey, decryption)
+    this.setState({ decrypted })
   }
 
   async encrypt(e) {
     e.preventDefault()
-    console.log('publivc key', this.state.publicKey)
-    let encrypted = await EthCrypto.encryptWithPublicKey(this.state.publicKey, 'test')
-    console.log(encrypted)
+    let encrypted = await encryptWithPublicKey(this.state.publicKey, 'test')
+    encrypted = JSON.stringify(encrypted)
+    this.setState({ encrypted })
   }
 
   getKey(e) {
@@ -40,8 +44,10 @@ class EncryptAndDecrypt extends Component {
     return (
       <div>
         <div>{this.state.publicKey}</div>
+        <div>{this.state.encrypted}</div>
+        <div>{this.state.decrypted}</div>
         <button onClick={this.encrypt.bind(this)}>Encrypt</button>
-        <button onClick={this.decrypt}>Decrypt</button>
+        <button onClick={this.decrypt.bind(this)}>Decrypt</button>
         <button onClick={this.getKey.bind(this)}>get key</button>
         <button onClick={this.getAddress}>get address</button>
       </div>
