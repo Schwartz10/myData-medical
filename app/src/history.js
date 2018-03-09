c2bbd40d4cdb9ff6fad909667ceea9df653e580e
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-03-09 10:29:00 -0500

    fixes bug with reloading

--- app/src/history.js
+++ app/src/history.js
@@ -1,4 +1,4 @@
-import createHistory from 'history/createBrowserHistory'
+import createHistory from 'history/createHashHistory'
 import createMemoryHistory from 'history/createMemoryHistory'
 
 const history = process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory()
